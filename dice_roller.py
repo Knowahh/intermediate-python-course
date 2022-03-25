372af8c7d755086c5d41b1943a76dadc3bcc08e8
Knowahh <92389368+Knowahh@users.noreply.github.com>
2022-03-25 17:54:19 -0400

    rolls 2 dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,13 @@
+import random
+
 def main():
-  print('You rolled a die')
+  dicerolls = 2
+  dicesum = 0
+  for i in range(dicerolls):
+    roll = random.randint(1,6)
+    print(f'You rolled a {roll}')
+    dicesum += roll
+  print(f'You have rolled a total of {dicesum}')
 
 if __name__== "__main__":
   main()
